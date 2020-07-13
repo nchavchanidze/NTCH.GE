72a1049d6f538670a413db3dc1bcb10d6f256d32
Nikoloz Tchavtchanidze <45031920+nchavchanidze@users.noreply.github.com>
2020-07-13 23:53:30 +0400

    Update Socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -15,21 +15,21 @@ const iconStyle = {
 function Socials(props) {
     return (
         <div className="d-flex justify-content-between align-items-center pt-2 hero-socials">
-            <a href="https://www.facebook.com/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="https://www.facebook.com/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="socialsBG">
                 <FontAwesomeIcon style={iconStyle} icon={['fab', 'facebook-f']} />
             </a>
             
-            <a href="https://github.com/nchavchanidze" target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="https://github.com/nchavchanidze" target="_blank" rel="noopener noreferrer" className="socialsBG">
                 <FontAwesomeIcon style={iconStyle} icon={['fab', 'github']} />
             </a>
 
-            <a href="https://www.linkedin.com/in/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="https://www.linkedin.com/in/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="socialsBG" >
                 <FontAwesomeIcon style={iconStyle} icon={['fab', 'linkedin-in']} />
             </a>
 
-            <a href="../../assets/docs/CV.pdf" download target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="../../assets/docs/CV.pdf" download target="_blank" rel="noopener noreferrer" className="d-flex align-items-center socialsBG">
                 <FontAwesomeIcon style={iconStyle} icon={['fas', 'download']} />
-                    &nbsp; RESUME
+                  <span className="pl-2">RESUME </span>  
             </a>
         </div>
     )
