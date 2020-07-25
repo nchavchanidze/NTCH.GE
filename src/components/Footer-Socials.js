afa2906d3d7f6e962b60c5de937c4f604d7ef5d8
Nikoloz Tchavtchanidze <45031920+nchavchanidze@users.noreply.github.com>
2020-07-25 12:57:12 +0400

    Update Footer-Socials.js

--- src/components/Footer-Socials.js
+++ src/components/Footer-Socials.js
@@ -7,29 +7,37 @@ import { fas, faDownload } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fab, fas, faFacebookF, faGithub, faLinkedinIn, faDownload)
 
+const BgColor = {
+    backgroundColor: '#fff',
+    color: '#ff0f5d'
+}
+const spanColor = {
+    color: '#ff0f5d'
+}
+
 const iconStyle = {
-    color: '#ffffff',
+    color: '#ff0f5d',
     fontSize: "1.33333333em !important"
 }
 
 function FooterSocials(props) {
     return (
         <div className="d-flex justify-content-between align-items-center footer-socials pt-2 ">
-            <a href="https://www.facebook.com/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="https://www.facebook.com/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="socialsBG " style={BgColor}>
                 <FontAwesomeIcon style={iconStyle} icon={['fab', 'facebook-f']} />
             </a>
             
-            <a href="https://github.com/nchavchanidze" target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="https://github.com/nchavchanidze" target="_blank" rel="noopener noreferrer" className="socialsBG " style={BgColor}>
                 <FontAwesomeIcon style={iconStyle} icon={['fab', 'github']} />
             </a>
 
-            <a href="https://www.linkedin.com/in/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="https://www.linkedin.com/in/nikoloztchavtchanidze/" target="_blank" rel="noopener noreferrer" className="socialsBG " style={BgColor}>
                 <FontAwesomeIcon style={iconStyle} icon={['fab', 'linkedin-in']} />
             </a>
 
-            <a href="../../assets/docs/CV.pdf" download target="_blank" rel="noopener noreferrer" className="p-0 m-0">
+            <a href="../../assets/docs/CV.pdf" download target="_blank" rel="noopener noreferrer" className="socialsBG " style={BgColor}>
                 <FontAwesomeIcon style={iconStyle} icon={['fas', 'download']} />
-                    &nbsp; RESUME
+                    <span style={spanColor} >&nbsp; RESUME</span>
             </a>
         </div>
     )
